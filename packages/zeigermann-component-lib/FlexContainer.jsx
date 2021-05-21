d90070b14103ddf6fb89abdd7625a10768d6f1ab
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-05-21 11:13:00 +0200

    more elaborated lib layout

--- packages/zeigermann-component-lib/FlexContainer.jsx
+++
@@ -1,5 +0,0 @@
-export function FlexContainer({ children, center = false }) {
-    return (<div className={`flex ${center ? "justify-center" : "justify-between"} border-gray-900 border-2 m-2 overflow-hidden`}>
-      {children}
-    </div>);
-}
--- packages/zeigermann-component-lib/LoadingIndicator.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function LoadingIndicator({ title }) {
-    return (<h1>{title}</h1>);
-}
