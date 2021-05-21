5145bd683553710c27f022b500950f2906a8fe30
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-05-21 11:12:38 +0200

    delete react showcase for now

--- packages/react-showcase/src/lib/LoadingIndicator.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = Readonly<{ title: string }>;
-
-export default function LoadingIndicator({ title }: Props) {
-  return (
-        <h1>{title}</h1>
-  );
-}
--- packages/react-showcase/test/regression/App.visual.test.ts
+++
@@ -1,8 +0,0 @@
-import { takeSnapshot } from 'testcafe-blink-diff';
-
-fixture `App Visual Regression Test`
-    .page `http://localhost:3000`;
-
-test("start", async t => {
-  await takeSnapshot(t);
-});
