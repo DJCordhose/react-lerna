8f63b7dcc2a7b9b24eae0ffe5899b256ac119009
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-05-22 18:34:00 +0200

    broken lib import removed

--- packages/module-webpack-js/src/index.js
+++ packages/module-webpack-js/src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { AriaButton, sayHi} from "zeigermann-component-lib";
+// We have Webpack 5, might need export maps to make this work
+// import { AriaButton, sayHi} from "zeigermann-component-lib";
 
 class HelloMessage extends React.Component {
     render() {
       return (
         <div>
           <p>Hiho, {this.props.name}</p>
-          <p><AriaButton testid='oha' label='Click mich' text='Click mich' onClick={() => console.log(sayHi("Olli"))} /></p>
+          {/* <p><AriaButton testid='oha' label='Click mich' text='Click mich' onClick={() => console.log(sayHi("Olli"))} /></p> */}
         </div>
       );
     }
