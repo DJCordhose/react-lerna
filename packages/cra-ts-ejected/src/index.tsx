a8edefe630d882e6e7a2e29bd292122b3881c7d2
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-06-01 14:47:38 +0200

    global style from lib included

--- packages/cra-ts-ejected/src/index.tsx
+++ packages/cra-ts-ejected/src/index.tsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'zeigermann-component-lib/dist/index.cjs.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
