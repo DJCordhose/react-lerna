94a71995500222f3311224deb7bed4ed7ec180c2
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2021-06-04 11:37:46 +0200

    tests: click test

--- packages/zeigermann-component-lib/src/AriaButton.test.tsx
+++ packages/zeigermann-component-lib/src/AriaButton.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { AriaButton } from "./AriaButton";
 
-test("smoke: renders something", () => {
+test("smoke: renders its text", () => {
   const label = "Click mich";
   const { getAllByText } = render(
     <AriaButton
@@ -14,4 +15,20 @@ test("smoke: renders something", () => {
   );
 
   expect(getAllByText(label)).toHaveLength(1);
+});
+
+test("click trigges callback", () => {
+  const onClick = jest.fn()
+  const label = "Click mich";
+  render(
+    <AriaButton
+      testid="oha"
+      label={label}
+      text={label}
+      onClick={onClick}
+    ></AriaButton>
+  );
+
+  userEvent.click(screen.getByText(label));
+  expect(onClick).toHaveBeenCalled();
 });
